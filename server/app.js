cf8e2c400484dcb37c22cfba9cadebb2e04ceaf0
HamzaHarrass <hamzaharrass05@gmail.com>
2024-01-15 09:21:25 +0100

    instalation de l'environement

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+// require("./config/DBconnection")();
+const cookieParser = require('cookie-parser');
+const app = express();
+// const authRouter = require("./routes/authRouter");
+// const appartementRoutes = require('./routes/appartementRouter');
+// const clientRouter = require('./routes/clientRouter');
+// const paiementRouter = require('./routes/paiementRouter');
+
+const cors = require('cors');
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+require('dotenv').config();
+
+app.use(cors({
+    origin: "http://localhost:5170"
+}));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "ALL IS GOOD" })
+});
+
+// prefix | suffix
+// app.use("/auth", authRouter);
+// app.use('/api', appartementRoutes);
+// app.use('/api', clientRouter);
+// app.use('/api', paiementRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
