1f7176026efa92a4fb6f764b98d552be6af4a70d
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-05 16:12:53 +0100

    changement sur shcema de cart et CartController

--- server/Controllers/CartController.js
+++ server/Controllers/CartController.js
@@ -2,35 +2,36 @@ const Cart = require("../Models/Cart");
 const Produit = require("../Models/Produit");
 
 const addToCart = async (req, res) => {
-  try {
-    const { produitId, quantity } = req.body;
-
-    if (!produitId || !quantity) {
-      return res.status(400).json({ message: 'Both produitId and quantity are required.' });
+    try {
+      const { produitId, quantity, status } = req.body;
+  
+      if (!produitId || !quantity) {
+        return res.status(400).json({ message: 'Both produitId and quantity are required.' });
+      }
+  
+      const produit = await Produit.findById(produitId);
+      if (!produit) {
+        return res.status(404).json({ message: 'Product not found.' });
+      }
+  
+      let cartItem = await Cart.findOne({ produit: produitId });
+      if (cartItem) {
+        cartItem.quantity += parseInt(quantity);
+      } else {
+        cartItem = new Cart({
+          produit: produitId,
+          quantity: parseInt(quantity),
+          status: status || 'pending' 
+        });
+      }
+  
+      await cartItem.save();
+  
+      res.status(200).json({ message: 'Product added to cart successfully', cartItem });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
     }
-
-    const produit = await Produit.findById(produitId);
-    if (!produit) {
-      return res.status(404).json({ message: 'Product not found.' });
-    }
-
-    let cartItem = await Cart.findOne({ produit: produitId });
-    if (cartItem) {
-      cartItem.quantity += parseInt(quantity);
-    } else {
-      cartItem = new Cart({
-        produit: produitId,
-        quantity: parseInt(quantity)
-      });
-    }
-
-    await cartItem.save();
-
-    res.status(200).json({ message: 'Product added to cart successfully', cartItem });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-};
+  };
 
 module.exports = { addToCart };
