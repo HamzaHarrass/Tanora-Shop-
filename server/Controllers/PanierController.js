9802c1d37604ad9a33b0d47c33274a44ee8d714b
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-06 11:19:48 +0100

    ajouter panierController et crées la premier function qui get allCart

---
+++ server/Controllers/PanierController.js
@@ -0,0 +1,13 @@
+const Cart = require("../Models/Cart");
+
+const getCartItems = async (req, res) => {
+  try {
+    const cartItems = await Cart.find();
+    res.status(200).json(cartItems);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = { getCartItems };
