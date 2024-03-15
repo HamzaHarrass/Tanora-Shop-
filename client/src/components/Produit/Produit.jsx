dfc650815232e13fc8bad3eff7f8031fccf07bfa
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-15 14:58:38 +0000

    changé le color de bouton et style de popups

--- client/src/components/Produit/Produit.jsx
+++ client/src/components/Produit/Produit.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import logo from '../../assets/image/1e2f5cd0-2280-4816-ac4d-ffcbeabeb8c9.png'; 
 import axios from 'axios';
 axios.defaults.withCredentials = true;
+
 const Produit = () => {
   const [products, setProducts] = useState([]);
   const [showAddPopup, setShowAddPopup] = useState(false);
@@ -75,71 +76,103 @@ const Produit = () => {
           </a>
         </div>
       </nav>
-      <div>
-        <h1 className="title">Product Management System</h1>
+
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-bold mt-8 mb-4">Product Management System</h1>
+
+        <div className="product-list">
+          <h2 className="text-xl font-semibold mb-4">Products</h2>
+          <button onClick={() => setShowAddPopup(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Add Product
+          </button>
+
+          <table className="w-full border-collapse mt-4">
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={index} className="border border-gray-300">
+                  <td className="border border-gray-300 px-4 py-2">{product.name}</td>
+                  <td className="border border-gray-300 px-4 py-2">{product.size}</td>
+                  <td className="border border-gray-300 px-4 py-2">{product.prix} DH</td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    <img src={`http://localhost:3000/uploads/${product.image}`} alt={product.name} className="h-12 w-12 object-cover" />
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    <button onClick={() => { setUpdateProduct({ id: product._id, name: product.name, size: product.size, prix: product.prix, image: product.image }); setShowUpdatePopup(true); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-2">
+                      Update
+                    </button>
+                    <button onClick={() => handleDeleteProduct(product._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Add Product Popup */}
+        {showAddPopup && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+            <div className="bg-white rounded-lg p-8 max-w-md w-full">
+              <h3 className="text-lg font-semibold mb-4">Add Product</h3>
+              <form encType="multipart/form-data">
+                <div>
+                  <label htmlFor="name">Name:</label>
+                  <input type="text" id="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                
+                </div>
+                <div>
+                <label htmlFor="size">Size:</label>
+                <input type="text" id="size" value={newProduct.size} onChange={(e) => setNewProduct({ ...newProduct, size: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                </div>
+                <div>
+                <label htmlFor="prix">Price:</label>
+                <input type="text" id="prix" value={newProduct.prix} onChange={(e) => setNewProduct({ ...newProduct, prix: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                </div>
+                <div>
+                <label htmlFor="image">Image:</label>
+                <input type="file" id="image" onChange={(e) => setNewProduct({ ...newProduct, image: e.target.files[0] })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                </div>
+                <div className="flex justify-end">
+                  <button type="button" onClick={handleAddProduct} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+                    Add Product
+                  </button>
+                  <button type="button" onClick={() => setShowAddPopup(false)} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    Close
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+
+        {/* Update Product Popup */}
+        {showUpdatePopup && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+            <div className="bg-white rounded-lg p-8 max-w-md w-full">
+              <h3 className="text-lg font-semibold mb-4">Update Product</h3>
+              <form encType="multipart/form-data">
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" value={updateProduct.name} onChange={(e) => setUpdateProduct({ ...updateProduct, name: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                <label htmlFor="size">Size:</label>
+                <input type="text" id="size" value={updateProduct.size} onChange={(e) => setUpdateProduct({ ...updateProduct, size: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                <label htmlFor="prix">Price:</label>
+                <input type="text" id="prix" value={updateProduct.prix} onChange={(e) => setUpdateProduct({ ...updateProduct, prix: e.target.value })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                <label htmlFor="image">Image:</label>
+                <input type="file" id="image" onChange={(e) => setUpdateProduct({ ...updateProduct, image: e.target.files[0] })} className="border border-gray-300 px-3 py-1 mb-2 rounded" />
+                <div className="flex justify-end">
+                  <button type="button" onClick={handleUpdateProduct} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+                    Update Product
+                  </button>
+                  <button type="button" onClick={() => setShowUpdatePopup(false)} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    Close
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className="product-list">
-        <h2 className="text-xl font-semibold mb-4">Products</h2>
-        <button onClick={() => setShowAddPopup(true)}>Add Product</button>
-
-        <table className="w-full border-collapse">
-          <tbody>
-            {products.map((product, index) => (
-              <tr key={index} className="border border-gray-300">
-                <td className="border border-gray-300 px-4 py-2">{product.name}</td>
-                <td className="border border-gray-300 px-4 py-2">{product.size}</td>
-                <td className="border border-gray-300 px-4 py-2">{product.prix} DH</td>
-                <td className="border border-gray-300 px-4 py-2">  <img src={`http://localhost:3000/uploads/${product.image}`} alt={product.name} className="h-12 w-12 object-cover" /></td> 
-                <td className="border border-gray-300 px-4 py-2">
-                  <button onClick={() => { setUpdateProduct({ id: product._id, name: product.name, size: product.size, prix: product.prix, image: product.image }); setShowUpdatePopup(true); }}>Update</button>
-                  <button onClick={() => handleDeleteProduct(product._id)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-{showAddPopup && (
-  <div className="popup">
-    <div className="popup-content">
-      <h3>Add Product</h3>
-      <form encType="multipart/form-data">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })} />
-        <label htmlFor="size">Size:</label>
-        <input type="text" id="size" value={newProduct.size} onChange={(e) => setNewProduct({ ...newProduct, size: e.target.value })} />
-        <label htmlFor="prix">Price:</label>
-        <input type="text" id="prix" value={newProduct.prix} onChange={(e) => setNewProduct({ ...newProduct, prix: e.target.value })} />
-        <label htmlFor="image">Image:</label>
-        <input type="file" id="image" onChange={(e) => {setNewProduct({ ...newProduct, image: e.target.files[0] }); console.log(e)}} />
-        <button type="button" onClick={handleAddProduct}>Add Product</button>
-        <button type="button" onClick={() => setShowAddPopup(false)}>Close</button>
-      </form>
-    </div>
-  </div>
-)}
-
-{showUpdatePopup && (
-  <div className="popup">
-    <div className="popup-content">
-      <h3>Update Product</h3>
-      <form encType="multipart/form-data">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={updateProduct.name} onChange={(e) => setUpdateProduct({ ...updateProduct, name: e.target.value })} />
-        <label htmlFor="size">Size:</label>
-        <input type="text" id="size" value={updateProduct.size} onChange={(e) => setUpdateProduct({ ...updateProduct, size: e.target.value })} />
-        <label htmlFor="prix">Price:</label>
-        <input type="text" id="prix" value={updateProduct.prix} onChange={(e) => setUpdateProduct({ ...updateProduct, prix: e.target.value })} />
-        <label htmlFor="image">Image:</label>
-        <input type="file" id="image" onChange={(e) => setUpdateProduct({ ...updateProduct, image: e.target.files[0] })} />
-        <button type="button" onClick={handleUpdateProduct}>Update Product</button>
-        <button type="button" onClick={() => setShowUpdatePopup(false)}>Close</button>
-      </form>
-    </div>
-  </div>
-)}
     </>
   );
 };
