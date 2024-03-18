72b2d3bd97d99a4f8b03d5623b486d7796425b8c
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-18 14:46:34 +0000

    select your sweatshirt whit color

--- client/src/components/Design/design.jsx
+++ client/src/components/Design/design.jsx
@@ -194,7 +194,10 @@ function design() {
             {activePage == 'Done' && <Done/>}
         </div>
         <div>
-            <div className="shadow-lg p-5 rounded-2xl flex justify-center"> <img className="h-32" src="https://storage.googleapis.com/ecommerce-main-e0cf7.appspot.com/products/62901d5f2fdf190016cf79ce/front.png" alt="" /></div>
+            <div className="shadow-lg p-5 rounded-2xl flex justify-center"> 
+            {selectedColor === 'white' && <img className="h-32" src={sweatshirtwhite} alt="sweatshirt blanc" />}
+          {selectedColor === 'black' && <img className="h-32" src={sweatshirtblack} alt="sweatshirt noir" />}
+            </div>
             <div className="shadow-lg p-5 rounded-2xl flex flex-col gap-2">
                 <button className="flex justify-center py-4 hover:bg-gray-200">
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" color="" height="40" width="40" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
