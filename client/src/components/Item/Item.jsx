c340083451ebaf17f9a8d817de3363f1b3100578
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-19 21:43:47 +0000

    distribuer code Item new component Item

---
+++ client/src/components/Item/Item.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Editor from "./Editor";
+
+function Item({ selectedColor, setSelectedColor }) {
+  return (
+    <>
+      <h3 className="text-2xl font-bold text-center">
+        Configure your item, its color, size, decoration, and quantity.
+      </h3>
+      <Editor setSelectedColor={setSelectedColor} selectedColor={selectedColor} />
+    </>
+  );
+}
+
+export default Item;
