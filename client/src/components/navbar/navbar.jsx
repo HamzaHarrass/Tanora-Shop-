dcb08c0cc89e5a042b1892a73bcb90c2b69ec28c
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-21 10:14:21 +0000

    modification sur navbar effacer contact et ajouter order

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -56,8 +56,10 @@ function navbar() {
                   </Link>
               </li>
               <li>
-                <a href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 border-b text-base border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                <Link to={`/order`} 
+                  className="block py-2 pl-3 pr-4 text-gray-700 border-b text-base border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
+                    Order
+                </Link>
               </li>
             </ul>
           </div>
