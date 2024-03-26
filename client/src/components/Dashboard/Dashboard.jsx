a3c11f4743857983e04af036f4719535084a927c
HamzaHarrass <hamzaharrass05@gmail.com>
2024-03-26 14:43:42 +0000

    changer style de table sur component Dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -139,14 +139,39 @@ const Produit = () => {
                   <td class="pr-0 text-start">
                     <span class="font-semibold text-light-inverse text-md/normal">{product.prix} DH</span>
                   </td>
-                  <td className="text-center  px-4 py-2">
-                    <button onClick={() => { setUpdateProduct({ id: product._id, name: product.name, size: product.size, prix: product.prix, image: product.image, category: product.category, color: product.color }); setShowUpdatePopup(true); }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-2">
-                      Update
-                    </button>
-                    <button onClick={() => handleDeleteProduct(product._id)} className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
-                      Delete
-                    </button>
-                  </td>
+                  <td class="px-16 py-2">
+              <span class="text-yellow-500 flex">
+              <svg
+                    onClick={() => { setUpdateProduct({ id: product._id, name: product.name, size: product.size, prix: product.prix, image: product.image, category: product.category, color: product.color }); setShowUpdatePopup(true); }}
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 text-green-700 mx-2"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                  <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"
+                  />
+              </svg>
+              <svg
+                    onClick={() => handleDeleteProduct(product._id)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5 text-red-700"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+              <path                               
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                  />
+              </svg>
+              </span>
+            </td>
                 </tr>
                               ))}
               </tbody>
